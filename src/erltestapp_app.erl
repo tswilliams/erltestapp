c9c41f011c66f80c4485479e2fbd14744268c9a6
Tom Williams <tswilliams@users.noreply.github.com>
2014-12-05 11:48:05 +0000

    Remove redundant application start call

--- src/erltestapp_app.erl
+++ src/erltestapp_app.erl
@@ -10,7 +10,7 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    application:start(lager),
+%    application:start(lager),
     erltestapp_sup:start_link().
 
 stop(_State) ->
